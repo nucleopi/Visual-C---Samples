66055d9e6dfe781861e361cb39a927ad345b9949
nucleopi <bogdanmihalcea@yahoo.com>
2017-02-11 22:05:13 -0800

    Add license

--- iSpaceExplorer/ir_base.cpp
+++ iSpaceExplorer/ir_base.cpp
@@ -1,3 +1,11 @@
+/****************************************************************************************************************/
+/*                                                                                                              */ 
+/*                                                                                                              */
+/*   Bogdan Mihalcea                                                                                            */ 
+/*   bogdanmihalcea@yahoo.com                                                                                   */
+/*                                                                                                              */  
+/****************************************************************************************************************/
+
 #include "ir_base.h"
 
 ir_base::ir_base(std::string name, uint32_t dopin, uint32_t dipin) : sensor_transmission_base(name, dopin, dipin) {}
