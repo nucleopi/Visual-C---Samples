e3c2a78b150f60fae9cecc1a87a37a8ff6719256
nucleopi <bogdanmihalcea@yahoo.com>
2017-02-21 13:59:31 -0800

    Revert "Add license"
    This reverts commit 66055d9e6dfe781861e361cb39a927ad345b9949.

--- iSpaceExplorer/ir_base.cpp
+++ iSpaceExplorer/ir_base.cpp
@@ -1,11 +1,3 @@
-/****************************************************************************************************************/
-/*                                                                                                              */ 
-/*                                                                                                              */
-/*   Bogdan Mihalcea                                                                                            */ 
-/*   bogdanmihalcea@yahoo.com                                                                                   */
-/*                                                                                                              */  
-/****************************************************************************************************************/
-
 #include "ir_base.h"
 
 ir_base::ir_base(std::string name, uint32_t dopin, uint32_t dipin) : sensor_transmission_base(name, dopin, dipin) {}
